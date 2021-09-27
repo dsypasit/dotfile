d9eb9dce0ee9ed7760c1c38ccfa3ac224db16765
Pasit Sri-intarasut <s6301012620065@kmutnb.ac.th>
2021-09-27 10:58:01 +0700

    backup

--- script/Jc.sh
+++ script/Jc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #cd $PWD
+#rm $1.class;
 javac $1.java;
 if [ $? -eq 0 ]; then
-	#name=$( awk -F '.' '{print $1}' <<< $1);
+	name=$( awk -F '.' '{print $1}' <<< $1);
 	java $1
 else
 	echo "Error"
