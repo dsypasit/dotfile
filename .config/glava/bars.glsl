5f0c64fa915691ba0209c3bb482a57c4163eb9dd
dsypasit <ong.pasit@gmail.com>
2022-07-12 13:26:23 +0700

    fix color

--- .config/glava/bars.glsl
+++ .config/glava/bars.glsl
@@ -2,7 +2,7 @@
 /* Center line thickness (pixels) */
 #define C_LINE 1
 /* Width (in pixels) of each bar */
-#define BAR_WIDTH 4
+#define BAR_WIDTH 8
 /* Width (in pixels) of each bar gap */
 #define BAR_GAP 2
 /* Outline color */
@@ -20,8 +20,8 @@
 /* Bar color changes with height */
 #define GRADIENT (d / GRADIENT_POWER + 1)
 /* Bar color */
-//#define COLOR (#D9F8C4 * GRADIENT)
-#define COLOR mix(#D9F8C4, #a0a0b2, clamp(d / 80, 0, 1))
+//#define COLOR (#231955 * GRADIENT)
+#define COLOR mix(#F47C7C, #FAD4D4, clamp(d / 80, 0, 1))
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
 #define DIRECTION 0
 /* Whether to switch left/right audio buffers */
