c627f580f05e37724af9be48ef3bfb879540bdda
pasit.sri <ong.pasit@gmail.com>
2024-04-22 19:58:25 +0700

    update nixos config

--- configuration.nix
+++ configuration.nix
@@ -4,190 +4,222 @@
 
 { config, lib, pkgs, ... }:
 let
-  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
 in
 {
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      packageOverrides = pkgs: {
+        unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+      };
+    };
+  };
   programs.hyprland = {
     enable = true;
-xwayland.enable=true;
-nvidiaPatches = true;
+    xwayland.enable=true;
+    nvidiaPatches = true;
   };
 
-services.greetd = {
-enable = true;
-settings = {
-default_session = {
-command = "${tuigreet} --time --remember --cmd Hyprland";
-user = "dsy";
-};
-};
-};
-
-fonts.packages = with pkgs; [
-  noto-fonts
-  noto-fonts-cjk
-  noto-fonts-emoji
-  liberation_ttf
-  fira-code
-  fira-code-symbols
-  mplus-outline-fonts.githubRelease
-  dina-font
-  proggyfonts
-fira-code-nerdfont
-];
+  fonts.packages = with pkgs; [
+    noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      fira-code-nerdfont
+  ];
 
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
+# Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "dsy"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+# Pick only one of the below networking options.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  time.timeZone = "Asia/Bangkok";
+# Set your time zone.
+    time.timeZone = "Asia/Bangkok";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+# Select internationalisation properties.
+# i18n.defaultLocale = "en_US.UTF-8";
+# console = {
+#   font = "Lat2-Terminus16";
+#   keyMap = "us";
+#   useXkbConfig = true; # use xkb.options in tty.
+# };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+# Enable the X11 windowing system.
+# services.xserver.enable = true;
 
 
-  
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+# Configure keymap in X11
+# services.xserver.xkb.layout = "us";
+# services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Enable sound.
+# Enable CUPS to print documents.
+# services.printing.enable = true;
+
+# Enable sound.
   sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+# hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.dsy = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "docker"]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       firefox
-       tree
-     ];
-   };
+# Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dsy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker"]; # Enable ‘sudo’ for the user.
+      packages = with pkgs; [
+      firefox
+        tree
+      ];
+  };
 
-virtualisation.docker.enable = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     kitty
-     swww
-     waybar
-     mako
-     blueman
-     xfce.thunar
-     swappy
-     grim
-     wofi
-     wofi-emoji
-     wl-clipboard
-     swaylock
-     cliphist
-     fzf
-     bfs
-     lazygit
-     neovim
-     lua
-     nodejs_21
-     python3
-     starship
-     tmux
-     libgcc
-     gcc
-     ripgrep
-     fd
-     ranger
-     yazi
-     pavucontrol
-     rofi
-     bat
-     jq
-     go
-     rustup
-   ];
+  virtualisation.docker.enable = true;
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  environment.sessionVariables = rec {
+      BROWSER = "${pkgs.firefox}/bin/firefox";
+    };
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+      kitty
+      swww
+      waybar
+      mako
+      blueman
+      xfce.thunar
+      swappy
+      grim
+      wofi
+      wofi-emoji
+      wl-clipboard
+      swaylock
+      cliphist
+      fzf
+      bfs
+      lazygit
+      neovim
+      lua
+      nodejs_21
+      python3
+      starship
+      tmux
+      libgcc
+      gcc
+      ripgrep
+      fd
+      ranger
+      unstable.yazi
+      pavucontrol
+      rofi
+      bat
+      jq
+      go
+      rustup
+      lsd
+      wayland-scanner
+      unzip
+      gnumake
+      unstable.ollama
+      xdg-utils
+      ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    zlib
+    nss
+    openssl
+    curl
+    expat
+    # ...
+  ];
 
-  # List services that you want to enable:
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+# List services that you want to enable:
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+# Copy the NixOS configuration file and link it from the resulting system
+# (/run/current-system/configuration.nix). This is useful in case you
+# accidentally delete configuration.nix.
+# system.copySystemConfiguration = true;
+
+# This option defines the first version of NixOS you have installed on this particular machine,
+# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+#
+# Most users should NEVER change this value after the initial install, for any reason,
+# even if you've upgraded your system to a new NixOS release.
+#
+# This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+# so changing it will NOT upgrade your system.
+#
+# This value being lower than the current NixOS release does NOT mean your system is
+# out of date, out of support, or vulnerable.
+#
+# Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+# and migrated your data accordingly.
+#
+# For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
 
 # rtkit is optional but recommended
-security.rtkit.enable = true;
-services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true;
-  # If you want to use JACK applications, uncomment this
-  #jack.enable = true;
-};
+    security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+# If you want to use JACK applications, uncomment this
+#jack.enable = true;
+  };
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${tuigreet} --time --remember --cmd Hyprland";
+        user = "dsy";
+      };
+    };
+  };
+
 }
 
